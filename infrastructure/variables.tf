5bcc6798ad63c8a4e98ce728c76d1f9c4a49ab41
LeonardBarrera <96848062+LeonardBarrera@users.noreply.github.com>
2024-09-27 10:54:05 -0500

    Initial Commit
    Initial Commit

---
+++ infrastructure/variables.tf
@@ -0,0 +1,55 @@
+variable "resource_group_name" {
+  type    = string
+  default = "eiad"
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_contributors" {
+  type        = list(string)
+  description = "List of Object IDs of AAD principals / groups to be given contribute access to the RG"
+  default     = []
+}
+
+variable "azuread_object_owners" {
+  type        = list(string)
+  description = "List of Object IDs or AAD Users to be made owners of Azure AD objects created for eIAD"
+  default     = []
+}
+
+variable "tags" {
+  default = {
+    "ProjectName" = "eIAD"
+  }
+}
+
+variable "is_local" {
+  type    = bool
+  default = false
+}
+
+variable "build_number" {
+  type        = string
+  default     = "local"
+  description = "Build Number from Azure DevOps"
+}
+
+variable "node_count" {
+  type        = number
+  default     = 14
+  description = "Number of nodes to allocate to the spark pool"
+}
+
+variable "partner_id" {
+  type        = string
+  default     = ""
+  description = "Azure customer usage attribution"
+}
+
+variable "deploy_customer_attribution" {
+  type        = bool
+  default     = true
+  description = "Azure customer usage attribution flag"
+}
